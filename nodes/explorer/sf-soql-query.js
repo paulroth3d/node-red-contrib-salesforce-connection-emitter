d75e41c56484a01332fe61867693b327e91a58de
Paul Roth <paulroth3d@gmail.com>
2019-05-05 17:28:33 -0500

    IMPORT: initial query node

---
+++ nodes/explorer/sf-soql-query.js
@@ -0,0 +1,58 @@
+const log = require('fancy-log'); // eslint-disable-line no-unused-vars
+
+/**
+ * Node Red Node to run a SOQL Query
+ */
+class SoqlQueryNode {
+
+  /** Constructor */
+  constructor() {
+    // super();
+
+    //-- initialize component properties
+  }
+  
+  /**
+   * Initialize the node red node
+   * @param {object} RED - Node Red framework
+   * @param {object} config - configuration for module from the node red editor
+   * @param {object} nodeRedNode - the node red instance
+   */
+  initialize(RED, config, nodeRedNode) {
+    // super.initialize(RED, config, nodeRedNode);
+
+    //-- capture information from the nodeRedNode
+    this.name = nodeRedNode.name;
+
+    //-- handle events on the nodeRedNode
+    nodeRedNode.on('input', function(msg){
+      // msg.payload = node.query;
+
+      nodeRedNode.send(msg);
+    });
+
+    return this;
+  }
+}
+
+/**
+ * Initialize node-red node module
+ * @param {NodeRed} RED - Node Red framework instance
+ */
+function setupNodeRed(RED){
+  RED.nodes.registerType('sf-soql-query', function(config){
+    RED.nodes.createNode(this, config);
+
+    //-- capture information from the config
+    this.name = config.name;
+
+    this.info = new SoqlQueryNode()
+      .initialize(RED, config, this)
+      .listenToConnection('sfconn');
+  });
+}
+
+//-- because it seems we cannot export the class outright...
+setupNodeRed.infoClass = SoqlQueryNode;
+
+module.exports = setupNodeRed;
