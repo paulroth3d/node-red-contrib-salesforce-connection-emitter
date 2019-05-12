b326d69952dea0a4913657954da061150298c4d5
Paul Roth <paulroth3d@gmail.com>
2019-05-11 20:09:29 -0500

    IMPORT: initial universal http call

--- test/universal-http_spec.js
+++ test/universal-http_spec.js
@@ -3,23 +3,30 @@ const log = require('fancy-log'); // eslint-disable-line no-unused-vars
 //-- asserts for mocha tests / others are also available.
 const {assert,expect} = require('chai'); // eslint-disable-line no-unused-vars
 
-const GenerateLoopKeys = require('../nodes/iterate/generate-loop-keys').infoClass;
+const UniversalHttp = require('../nodes/http/sf-universal-http').infoClass;
 
 //-- helper for node red
 // const helper = require('node-red-node-test-helper');
 
 const testUtils = require('./util/TestUtils');
 
+//-- mock the connection
+let CONNECTION_EMITTER_MOCK;
+let CONNECTION_MOCK;
 //-- mock the node red node it will work with
 let NODE_MOCK;
 //-- mock node red
 let RED_MOCK; //-- to be reset each test, because each test should have its own unique getNode stub
+
 //-- mock the config passed to the node
 let CONFIG_MOCK;
 
-describe('generate-loop-keys', () => {
+describe('sf-universal-http', () => {
   beforeEach((done) => {
-    RED_MOCK = testUtils.createNodeRedMock();
+    //-- reset the red mock to ensure the getNode is specific to this test
+    RED_MOCK = testUtils.createNodeRedMock('sfconn-id', CONNECTION_EMITTER_MOCK);
+    CONNECTION_EMITTER_MOCK = testUtils.createConnectionEmitterMock();
+    CONNECTION_MOCK = CONNECTION_EMITTER_MOCK.info.connection;
     NODE_MOCK = testUtils.createNodeRedNodeMock();
     CONFIG_MOCK = {
       name:'node'
@@ -35,7 +42,7 @@ describe('generate-loop-keys', () => {
   });
   it('can initialize', () => {
     const testPromise = new Promise((resolve, reject) => {
-      const node = new GenerateLoopKeys();
+      const node = new UniversalHttp();
       node.initialize(RED_MOCK, CONFIG_MOCK, NODE_MOCK);
       resolve();
     });
